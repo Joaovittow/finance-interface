13829bd7859b1767329515846355b5d0ff68e26f
joaovittow <joaovitorseixasn13@outlook.com>
2025-12-01 13:42:16 -0300

    feat: Add API constants and environment variable example, and update gitignore to exclude .env files.

--- src/constants/apiEndpoints.js
+++ src/constants/apiEndpoints.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://finance-api-i1ix.onrender.com/api';
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
 
 export const ENDPOINTS = {
   MESES: {
